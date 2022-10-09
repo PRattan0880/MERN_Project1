0440fb7fac90570ef215a72e459b3d4722feb22d
Sharnpreet Singh <prattan0880@gmail.com>
2022-10-09 12:19:29 -0700

    updated item schema validation

--- server/models/Item.model.js
+++ server/models/Item.model.js
@@ -1,37 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const itemSchema = new Schema({
-    sku: Number,
-    name: String,
-    // quantity: Number,
-    category: String,
-    price: Number,
-    imageURL: String
 
-    // "warehouseNumber": Number,
-    // "MAX_CAPACITY": Number,
-    // "items": [
-    //     {
-    //         "sku": String,
-    //         "qty": Number,
-    //         "category": String,
-    //         "price": Number,
-    //         "description": String
-    //     }
-    // ]
-    // warehouseNo: Number,
-    // items: [{
-    //     upc: {
-    //         type: String,
-    //         unique: true
-    //     },
-    //     description: String,
-    //     category: String,
-    //     price: Number,
-    //     quantity: Number,
-    //     imageURL: String
-    // }]
+/**
+ * Creates an Item Schema and validation such as required value
+ * for document
+ */
+const itemSchema = new Schema({
+    sku: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+    }
 }, {
     toJson: { virtual: true },
     toObject: { virtual: true }
